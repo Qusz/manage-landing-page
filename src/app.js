e8e523bb61dec79121e7fa4d787f471990098671
Denis Gorchakov <32505327+Qusz@users.noreply.github.com>
2022-10-11 21:39:10 +0800

    reworked navbar logic

--- src/app.js
+++ src/app.js
@@ -1,15 +1,29 @@
 
+class Navbar {
+  constructor() {
+    this.hamburger = document.querySelector('.navbar__hamburger');
+    this.veil = document.querySelector('.veil');
+    this.navbar = document.querySelector('.navbar__items');
+  }
 
-loadHamburger();
-
-function loadHamburger() {
-  const hamburger = document.querySelector('.navbar__hamburger'),
-        navbarItems = document.querySelector('.navbar__items'),
-        body = document.querySelector('.body');
-  
-  hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('is-active');
-    navbarItems.classList.toggle('is-active');
-    body.classList.toggle('is-active');
-  })
+  toggleNav() {
+    this.hamburger.classList.toggle('is-active');
+    this.veil.classList.toggle('is-active');
+    this.navbar.classList.toggle('is-active');
+  }
 }
+
+(() => {
+  const navbar = new Navbar();
+
+  navbar.hamburger.addEventListener('click', () => {
+    navbar.toggleNav();
+  });
+
+  window.addEventListener('click', (e) => {
+    if (e.target.classList.contains('veil')) {
+      navbar.toggleNav();
+    }
+  });
+})();
+
